ca970181fdb461669bf1425f6662a36b0563986f
Peter Goes <petergoes@gmail.com>
2023-04-28 21:09:14 +0200

    Sort daat

--- src/get-data.js
+++ src/get-data.js
@@ -19,15 +19,33 @@ function buildRecord(record) {
   }
 
 
-  return `${parsedName.trim()} (${pageNumbers.join(', ')})`
+  return [parsedName.trim(), ...pageNumbers]
 }
 
+function buildSearchString(record) {
+  const [name, ...pageNumbers] = record
+  return `${name} (${pageNumbers.join(', ').trim()})`
+}
+
+function sort(a, b) {
+  if (Number(a[1]) < Number(b[1])) {
+    return -1;
+  }
+  if (Number(a[1]) > Number(b[1])) {
+    return 1;
+  }
+  // a must be equal to b
+  return 0;
+}
 
 export const getData = async () => {
   return fetch('/data.csv')
     .then(response => response.text())
     .then(data => data.split('\n'))
     .then(records => records.map(buildRecord))
+    .then(records => records.sort(sort))
+    .then(records => records.filter(x => x))
+    .then(records => records.map(buildSearchString))
     .then(records => records.flat())
     .then(records => records.filter(x => x))
     .then(records => [...new Set(records)])
